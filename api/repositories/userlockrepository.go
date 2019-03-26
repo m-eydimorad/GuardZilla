acc562c2383c0c6afa2f5ba6074d8508b2b0c5d7
Eydimorad Mohamad <M_Eydimorad@iranet.net>
2019-03-26 15:10:59 +0430

    add db backup file. refactor userlockrepository add method

--- api/repositories/userlockrepository.go
+++ api/repositories/userlockrepository.go
@@ -2,31 +2,11 @@ package repositories
 
 import (
 	"../models"
-	"strings"
 )
 
 func PostUserLock(userlock models.UserLock) bool {
-	sqlStr := "INSERT INTO userlock(userid, environmentid, comment) VALUES "
 	vals := []interface{}{}
-
-	sqlStr += "(?, ?, ?),"
 	vals = append(vals, &userlock.UserId, &userlock.EnvironmentId, &userlock.Comment)
-
-	//trim the last ,
-	sqlStr = strings.TrimSuffix(sqlStr, ",")
-
-	//Replacing ? with $n for postgres
-	sqlStr = ReplaceSQL(sqlStr, "?")
-
-	//prepare the statement
-	stmt, err := db.Prepare(sqlStr)
-	if err != nil {
-		return false
-	}
-	//format all vals at once
-	_, error := stmt.Exec(vals...)
-	if error != nil {
-		return false
-	}
-	return true
+	result:=ExecuteInsertSqlStatement("INSERT INTO userlock(userid, environmentid, comment)",vals)
+	return result
 }
--- api/repositories/utils.go
+++ api/repositories/utils.go
@@ -5,6 +5,25 @@ import (
 	"strings"
 )
 
+func ExecuteInsertSqlStatement(insertSqlStatemenet string,values []interface{}) bool{
+	sqlStr :=insertSqlStatemenet+ " VALUES "
+	sqlStr += "(?, ?, ?),"
+	//trim the last ,
+	sqlStr = strings.TrimSuffix(sqlStr, ",")
+	//Replacing ? with $n for postgres
+	sqlStr = ReplaceSQL(sqlStr, "?")
+	//prepare the statement
+	stmt, err := db.Prepare(sqlStr)
+	if err != nil {
+		return false
+	}
+	//format all vals at once
+	_, error := stmt.Exec(values...)
+	if error != nil {
+		return false
+	}
+	return true
+}
 func ReplaceSQL(old, searchPattern string) string {
 	tmpCount := strings.Count(old, searchPattern)
 	for m := 1; m <= tmpCount; m++ {
