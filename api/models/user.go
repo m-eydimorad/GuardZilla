1aa941d38d944f34df3d3445f08978726e6576ef
Eydimorad Mohamad <M_Eydimorad@iranet.net>
2019-03-27 09:59:17 +0430

    add firstname and lastname fields to db and user api

--- api/models/user.go
+++ api/models/user.go
@@ -3,6 +3,8 @@ package models
 type User struct {
 	Id          int    `json:"Id"`
 	Username      string    `json:"Username"`
+	FirstName string    `json:"FirstName"`
+	LastName string    `json:"LastName"`
 }
 
 func NewUser() *User {
